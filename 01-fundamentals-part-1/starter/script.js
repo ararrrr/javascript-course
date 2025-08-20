525e9d73e1a035a10121034ac3882beaa282570e
ararrrr <rr.sena38@gmail.com>
2025-08-20 14:14:52 +0800

    feat: implement operators and expressions
    - Add mathematical operators (+, -, *, /, **)
    - Implement assignment operators (+=, -=, *=, /=, ++, --)
    - Create comparison operators for boolean logic
    - Understand operator precedence and parentheses
    - Complete BMI Calculator coding challenge with real-world application

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,24 +1,83 @@
-// JavaScript Fundamentals - Part 1
-// We'll write our code here!
+// // JavaScript Fundamentals - Part 1
+// // We'll write our code here!
 
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
-console.log("GUMANA KA");
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+// console.log("GUMANA KA");
 
 
-console.log("=== VARIABLES ===");
+// console.log("=== VARIABLES ===");
 
-let firstName  ="Jonas";
-console.log(firstName);
+// let firstName  ="Jonas";
+// console.log(firstName);
 
-let age = 30;
-console.log(age);
-age = 50;
-console.log(age);
+// let age = 30;
+// console.log(age);
+// age = 50;
+// console.log(age);
 
-const birthYear = 1991;
-console.log(birthYear);
+// const birthYear = 1991;
+// console.log(birthYear);
 
-const PI = 3.1414;
-console.log(PI);
+// const PI = 3.1414;
+// console.log(PI);
+
+// var job = "programmer";
+// job = "Teacher";
+// console.log(job);
+
+// let lastName = "Doe";
+
+// // let 3years = 3;
+// // let jonas&matilda = "JM";
+// // let new = 27;
+
+// console.log("=== DATA TYPES ===");
+
+// age = 24;
+// console.log(age);
+// console.log(typeof age);
+
+// //String
+// let userName = "mark";
+// console.log(userName);
+// console.log(typeof userName);
+
+// //Boolean
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// console.log(typeof javascriptIsFun);
+
+// //Undefined
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// //Dyanmic variable
+// let dyanmicVariable = 23;
+// console.log(dyanmicVariable, typeof dyanmicVariable);
+
+// // Change to a string
+// dyanmicVariable = "Now I'm a string!";
+// console.log(dyanmicVariable, typeof dyanmicVariable);
+
+// // Change to a boolean
+// dyanmicVariable = true;
+// console.log(dyanmicVariable, typeof dyanmicVariable);
+
+
+
+/////
+//Basic Operators - Math operators
+console.log("=== MATH OPERATORS ===");
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+
+
+
+
 
