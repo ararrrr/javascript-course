7c0f84cec8baabfd3b83aa03b7f4e4b711251e3a
ararrrr <rr.sena38@gmail.com>
2025-08-27 11:59:35 +0800

    feat: add Part 2 starter files and documentation

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -264,39 +264,95 @@ console.log(n);
 
 //Equality - Strict
 
-const age = "18";
-if (age === 18) console.log("You just became an adult :D (strict)");
-if (age == 18) console.log("You just became an adult :D (loose)");
+// const age = "18";
+// if (age === 18) console.log("You just became an adult :D (strict)");
+// if (age == 18) console.log("You just became an adult :D (loose)");
 
-console.log("18" === 18);
-console.log("18" == 18);
-console.log(18 === 18);
+// console.log("18" === 18);
+// console.log("18" == 18);
+// console.log(18 === 18);
 
-// why == can be dangerous
-console.log("0" == 0);
-console.log(0 == false);
-console.log("0" == false);
-console.log(null == undefined); //special case
+// // why == can be dangerous
+// console.log("0" == 0);
+// console.log(0 == false);
+// console.log("0" == false);
+// console.log(null == undefined); //special case
 
-console.log("" == 0);
-console.log("   " == 0);
+// console.log("" == 0);
+// console.log("   " == 0);
 
-// best practice
-const favourite = Number(prompt("What's your favourite number?"));
-console.log(favourite);
-console.log(typeof favourite);
+// // best practice
+// const favourite = Number(prompt("What's your favourite number?"));
+// console.log(favourite);
+// console.log(typeof favourite);
 
-if (favourite === 23) {
-  console.log("Cool! 23 is an amazing number!");
-} else if (favourite === 7) {
-  console.log("7 is also a cool number");
-} else if (favourite === 9) {
-  console.log("9 is also a cool number");
-} else {
-  console.log("Number is not 23 or 7 or 9");
-}
+// if (favourite === 23) {
+//   console.log("Cool! 23 is an amazing number!");
+// } if (favourite == 23) {
+//   console.log("23 string is also a cool number");
+// } else if (favourite === 9) {
+//   console.log("9 is also a cool number");
+// } else {
+//   console.log("Number is not 23 or 7 or 9");
+// }
 
-if (favourite !== 23) console.log("Why not 23?");
+// if (favourite !== 23) console.log("Why not 23?");
+
+//Logical Operators
+// const hasDriversLicense = false;
+// const hasGoodVision = true;
+
+// console.log(` AND OPERATOR: ${hasDriversLicense && hasGoodVision}`);
+// console.log(` OR OPERATOR: ${hasDriversLicense || hasGoodVision}`);
+// console.log(` NOT OPERATOR: ${!hasDriversLicense}`);
+
+// const isTired = false;
+// console.log(hasDriversLicense && hasGoodVision && !isTired);
+
+// if (hasDriversLicense && hasGoodVision && !isTired) {
+//   console.log("Ronald is able to drive!");
+// } else {
+//   console.log("Someone else should drive...");
+// }
+
+// const age = 20;
+// const hasPermission = true;
+// const hasExperience = false;
+
+// if ((age >= 18 || hasPermission) && hasExperience) {
+//   console.log("Approved to drive");
+// } else {
+//   console.log("Not approved to drive");
+// }
+
+/////////////////////////////////////
+// The Conditional (Ternary) Operator
+
+// const age = 19;
+
+// // Basic ternary: condition ? valueIfTrue : valueIfFalse
+// const drink = age >= 18 ? "wine" : "water";
+// console.log(drink);
+
+// //if else statement
+// let drink2;
+// if (age >= 18) {
+//   drink2 = "wine";
+// } else {
+//   drink2 = "water";
+// }
+// console.log(drink2);
+
+// console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
+
+//////////////////////
+//Coding Challenge
+const bill = 275;
+
+
+const tip = bill <= 300 && bill >=50 ? bill * 0.15 : bill * 0.2;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`)
 
 
 
